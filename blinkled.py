a4bc7e9dbb53b7c4c013495d154883e3aaf1adfb
Megan <musicalmacdonald@gmail.com>
2018-02-16 00:02:17 -0700

    Add files via upload
    added race_led function

--- blinkled.py
+++ blinkled.py
@@ -1,9 +1,9 @@
 from gpiozero import LED
 from time import sleep
 
-def blink_led_once( blink_frequency ):
+def blink_led_once( blink_frequency, port ):
     """turns the led on for 1/2 the time specified in blink_frequency, then it off for the same amount of time."""
-    myLED=LED(17)
+    myLED=LED(port)
 
     myLED.on()
     sleep(blink_frequency /2)
@@ -15,6 +15,28 @@ def blink_led_forever():
     while True:
         blink_led_once(1)
 
+def race_led():
+    #led 1
+    blink_led_once(2, 18)
+    #led 2
+    blink_led_once(2, 23)
+    #led 3
+    blink_led_once(2, 25)
+    #led 4
+    blink_led_once(2, 12)
+    #led 5
+    blink_led_once(2, 16)
+    #led 6
+    blink_led_once(2, 20)
+    #led 7
+    blink_led_once(2, 21)
+    #led 8
+    blink_led_once(2, 26)
+    #led 9
+    blink_led_once(2, 19)
+    #led 10
+    blink_led_once(2, 13)
+
 if __name__ == "__main__":
-    print("This will make the LED blink forever")
-    blink_led_forever()
+    print("This will make the LED blink once")
+    blink_led_once(2, 18)
